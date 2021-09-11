0fb57bffb76d5ed2cb2c578f3bb5d1faa240f12a
Guilherme Agunzo <g.agunzo@gmail.com>
2021-09-11 12:33:11 -0300

    Adicionando imagem, card list, ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ interface GetImagesResponse {
 
 export default function Home(): JSX.Element {
   async function fetchImages({ pageParam = null }): Promise<GetImagesResponse> {
-    const { data } = await api.get('api/images', {
+    const { data } = await api.get('/api/images', {
       params: {
         after: pageParam,
       },
